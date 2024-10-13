02ba30e24781c79dceaf6ee35ecd8c261491a58d
Ayush <ayushnigam843@gmail.com>
2024-10-13 23:06:47 +0530

    Improved UI

--- scriptly/src/pages/scripts/Script.tsx
+++ scriptly/src/pages/scripts/Script.tsx
@@ -34,7 +34,7 @@ const Script = () => {
     return (
         <div className='m-4 gap-3 '>
             <div className='flex flex-col gap-3 sticky'>
-                <div className='flex justify-between sticky top-2 ' >
+                <div className='flex justify-between top-2 ' >
                     <div className='flex gap-3'>
                         <h3 className='font-sans text-4xl font-bold text-gray-800 ' >
                             Untitled
@@ -141,7 +141,7 @@ const Script = () => {
                                 Recent Contributions
                             </h4>
                             <div className='flex flex-col gap-3' >
-                                {Array(5).fill(0).map(() => {
+                                {Array(4).fill(0).map(() => {
                                     return <span className='flex justify-between bg-gray-100  gap-2 p-2 rounded-lg' >
                                         <div className='flex gap-2 items-center' >
                                             <div className='flex flex-col gap-1'>
@@ -163,8 +163,25 @@ const Script = () => {
                                         </span>
                                     </span>
                                 })}
-                                <button className='bg-indigo-500 p-2 rounded-md text-white' >
+                                <button className='flex gap-2 items-center justify-center bg-indigo-500 text-white text-md px-3 py-2 rounded-md' >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                    </svg>
+
                                     See All Contributions
+                                </button>
+                                <button className='flex gap-2 items-center justify-center bg-indigo-500 text-white text-md px-3 py-2 rounded-md' >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                                    </svg>
+                                    <h6>Contribute</h6>
+                                </button>
+                                <button className='flex gap-2 items-center justify-center bg-indigo-500 text-white text-md  px-3 py-2  rounded-md' >
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
+                                    </svg>
+                                    <h6>Zen Mode</h6>
                                 </button>
                             </div>
 

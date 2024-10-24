b7d156d5a001c869621e854d7711084e1a9e251c
Ayush <ayushnigam843@gmail.com>
2024-10-24 18:42:19 +0530

    Improved UI

--- scriptly/src/components/Tabs.jsx
+++ scriptly/src/components/Tabs.jsx
@@ -1,118 +1,67 @@
 import React from 'react'
 
 const Tabs = () => {
+
+    const tabs = [
+        {
+            svg: (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                </svg>
+            ),
+            name: 'Script',
+            isActive: true,
+            count: 34
+        },
+        {
+            svg: (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3 4.5h14.25M3 9h9.75M3 13.5h9.75m4.5-4.5v12m0 0-3.75-3.75M17.25 21 21 17.25" />
+                </svg>
+            ),
+            name: 'Requests',
+            isActive: false,
+            count: 45
+        },
+        {
+            svg: (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                </svg>
+            ),
+            name: 'Contributions',
+            isActive: false,
+            count: 45
+        },
+        {
+            svg: (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z" />
+                </svg>
+            ),
+            name: 'About',
+            isActive: false,
+        },
+    ]
+
     return (
-        <div>
-            <div className="sm:hidden">
-                <label htmlFor="Tab" className="sr-only">Tab</label>
+        <div className='flex flex-col gap-3'>
+            <div className='flex  border-gray-200 text-lg font-semibold text-gray-700 w-full bg-white p-2 rounded-xl' >
+                {tabs.map((tab, i) => {
+                    return <a
+                        href="#"
+                        className={`flex w-full justify-center items-center gap-2 p-2 hover:border-gray-300 hover:text-gray-700 ${tab.isActive ? 'bg-indigo-500 text-white rounded-xl' : null} ${i !== tabs.length - 1 && !tab.isActive ? 'border-r border-gray-400' : null}`}
+                    >
+                        {tab.svg}
+                        {tab.name}
+                        {
+                            tab.count && <span className={`rounded-full py-1 px-1.5 text-sm  ${!tab.isActive ? 'bg-indigo-500 text-white' : 'bg-white text-indigo-500'}`} >{tab.count}</span>
+                        }
 
-                <select id="Tab" className="w-full rounded-md border-gray-200">
-                    <option>Settings</option>
-                    <option>Messages</option>
-                    <option>Archive</option>
-                    <option select>Notifications</option>
-                </select>
+                    </a>
+                })}
             </div>
-
-            <div className="hidden sm:block">
-                <div className="border-b border-gray-200">
-                    <nav className="-mb-px flex gap-6" aria-label="Tabs">
-                        <a
-                            href="#"
-                            className="inline-flex shrink-0 items-center gap-2 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-5"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
-                                />
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                />
-                            </svg>
-
-                            Settings
-                        </a>
-
-                        <a
-                            href="#"
-                            className="inline-flex shrink-0 items-center gap-2 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-5"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
-                                />
-                            </svg>
-
-                            Messages
-                        </a>
-
-                        <a
-                            href="#"
-                            className="inline-flex shrink-0 items-center gap-2 border-b-2 border-transparent px-1 pb-4 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-5"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z"
-                                />
-                            </svg>
-
-                            Archive
-                        </a>
-
-                        <a
-                            href="#"
-                            className="inline-flex shrink-0 items-center gap-2 border-b-2 border-sky-500 px-1 pb-4 text-sm font-medium text-sky-600"
-                            aria-current="page"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-5"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5"
-                                />
-                            </svg>
-
-                            Notifications
-                        </a>
-                    </nav>
-                </div>
-            </div>
+            <hr />
         </div>
     )
 }
--- scriptly/src/pages/scripts/Script.tsx
+++ scriptly/src/pages/scripts/Script.tsx
@@ -149,7 +149,7 @@ const Script = () => {
         setShowTextarea(false);
     };
     return (
-        <div className={`flex flex-col gap-3 sticky ${cursorClass}`}>
+        <div className={`flex flex-col gap-3 sticky ${cursorClass} container mx-auto`}>
             <div className='flex justify-between top-2 ' >
                 <div className='flex gap-3'>
                     <h3 className='font-sans text-4xl font-bold text-gray-800 ' >
@@ -164,9 +164,6 @@ const Script = () => {
                 <div className='flex gap-2' >
                     <button className='flex gap-2 items-center bg-indigo-500 text-white text-md px-3 py-2 rounded-md' onClick={handlePinClick} >
                         <img src="/pin.png" alt="" width="24px" />
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m15 11.25 1.5 1.5.75-.75V8.758l2.276-.61a3 3 0 1 0-3.675-3.675l-.61 2.277H12l-.75.75 1.5 1.5M15 11.25l-8.47 8.47c-.34.34-.8.53-1.28.53s-.94.19-1.28.53l-.97.97-.75-.75.97-.97c.34-.34.53-.8.53-1.28s.19-.94.53-1.28L12.75 9M15 11.25 12.75 9" />
-                        </svg> */}
                         <h6>Pin</h6>
                     </button>
                     <button onClick={handleContributeClick} className='flex gap-2 items-center bg-indigo-500 text-white text-md px-3 py-2 rounded-md' >
@@ -185,125 +182,8 @@ const Script = () => {
             </div>
             <hr />
             <Tabs />
-            <div className='grid-cols-10 grid gap-4'>
-                <div className='col-span-2 sticky top-6 h-min bg-white rounded-md p-4 gap-3 flex flex-col shadow-md' >
-                    <div className='flex gap-1 flex-col'>
-                        <h4 className='text-md text-gray-600 font-medium flex items-center gap-1' >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-                            </svg>
-                            Author </h4>
-                        <span className='flex items-center gap-2' >
-                            {/* <img className="w-8 h-8 rounded-full" src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" alt="Rounded avatar" /> */}
-                            <p className='text-lg font-semibold text-gray-800' >Ayush Nigam</p>
-                        </span>
-                    </div>
-                    <hr />
-                    <div className='flex gap-1 flex-col'>
-                        <h4 className='text-md text-gray-600 font-medium flex items-center gap-1' >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m10.5 21 5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 0 1 6-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 0 1-3.827-5.802" />
-                            </svg>
-                            Language
-                        </h4>
-                        <span className='flex items-center gap-2' >
-                            {/* <img className="w-8 h-8 rounded-full" src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" alt="Rounded avatar" /> */}
-                            <p className='text-lg font-semibold text-gray-800' >English</p>
-                        </span>
-                    </div>
-                    <hr />
-                    <div className='flex gap-1 flex-col'>
-                        <h4 className='text-md text-gray-600 font-medium flex items-center gap-1' > <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
-                        </svg>
-                            Contributors
-                            {/* <span className='bg-indigo-500 rounded-full p-1 px-2.5 text-white text-sm' > 2
-                                </span>  */}
-                        </h4>
-                        <span className='flex items-center gap-2' >
-                            {/* <img className="w-8 h-8 rounded-full" src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" alt="Rounded avatar" /> */}
-                            <p className='text-lg font-semibold text-gray-800' > Ayush Nigam</p>
-                        </span>
-                    </div>
-                    <hr />
-                    <div className='flex gap-2 flex-col'>
-                        <h4 className='text-md text-gray-600 font-medium flex items-center gap-1' > <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 6h.008v.008H6V6Z" />
-                        </svg>
-                            Genres
-                        </h4>
-                        <div className='flex gap-3 flex-wrap' >
-                            {
-                                [
-                                    { genre: 'Horror', bg: 'bg-red-100', color: 'text-red-800' },
-                                    { genre: 'Action', bg: 'bg-blue-100', color: 'text-blue-800' },
-                                    { genre: 'Comedy', bg: 'bg-yellow-100', color: 'text-yellow-800' },
-                                    { genre: 'Drama', bg: 'bg-green-100', color: 'text-green-800' },
-                                    { genre: 'Sci-Fi', bg: 'bg-purple-100', color: 'text-purple-800' },
-                                    { genre: 'Fantasy', bg: 'bg-indigo-100', color: 'text-indigo-800' },
-                                    { genre: 'Thriller', bg: 'bg-orange-100', color: 'text-orange-800' },
-                                    { genre: 'Romance', bg: 'bg-pink-100', color: 'text-pink-800' },
-                                    { genre: 'Mystery', bg: 'bg-gray-100', color: 'text-gray-800' },
-                                    { genre: 'Animation', bg: 'bg-teal-100', color: 'text-teal-800' }
-                                ].map(({ genre, bg, color }) => {
-                                    return <p className='text-lg font-semibold text-gray-800 py-1 px-2 bg-gray-100 rounded-full' > # {genre}  </p>
-                                    // <span className={`flex items-center gap-3 text-sm rounded-full`} >
-                                    //     {/* <img className="w-8 h-8 rounded-full" src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" alt="Rounded avatar" /> */}
-                                    //     <p className='text-lg   p-1' >{genre}</p>
-                                    // </span>
-                                })
-                            }
-                        </div>
-                    </div>
-                    <hr />
-                    <div className='flex flex-col gap-4'>
-                        <h4 className='text-md text-gray-600 font-medium flex items-center gap-1' >
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                            </svg>
-                            Recent Contributions
-                        </h4>
-                        <div className='flex flex-col gap-3' >
-                            {Array(4).fill(0).map(() => {
-                                return <span className='flex justify-between bg-gray-100  gap-2 p-2 rounded-lg' >
-                                    <div className='flex gap-2 items-center' >
-                                        <div className='flex flex-col gap-1'>
-                                            <div className='flex gap-2 items-center' >
-                                                <img className="w-6 h-6 rounded-full" src="https://tecdn.b-cdn.net/img/new/avatars/2.webp" alt="Rounded avatar" />
-                                                <p className='font-semibold text-lg text-gray-800' >Ayush Nigam</p>
-                                                <a href="#123" className='underline'>#{Math.floor(Math.random() * 20) + 1}</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <span className='text-xs self-end ' >
-                                        23 Min Ago
-                                    </span>
-                                </span>
-                            })}
-                            <Link to="/contributions" className='flex gap-2 items-center justify-center bg-indigo-500 text-white text-md px-3 py-2 rounded-md' >
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                                </svg>
-                                See All Contributions
-                            </Link>
-                            <button onClick={handleContributeClick} className='flex gap-2 items-center justify-center bg-indigo-500 text-white text-md px-3 py-2 rounded-md' >
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                                </svg>
-                                <h6>Contribute</h6>
-                            </button>
-                            <button className='flex gap-2 items-center justify-center bg-indigo-500 text-white text-md  px-3 py-2  rounded-md' >
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
-                                </svg>
-                                <h6>Zen Mode</h6>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex flex-col gap-4 col-span-8' >
+            <div>
+                <div className='flex flex-col gap-4' >
                     {
                         contributions.map((contribution, index) =>
                             <div className='word-spacing-1 flex flex-col relative gap-1 bg-white shadow-md rounded-lg p-4' onClick={() => handleCardClick(index)}>

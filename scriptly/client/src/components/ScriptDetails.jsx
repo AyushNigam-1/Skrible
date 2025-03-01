5f834c334886aff3d1fb700c245a02c386f54c15
ayush <ayushnigam843@gmail.com>
2025-03-01 08:44:06 +0530

    Minimal UI Enhancement

--- scriptly/client/src/components/ScriptDetails.jsx
+++ scriptly/client/src/components/ScriptDetails.jsx
@@ -16,6 +16,28 @@ const ScriptDetails = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                     </svg>
+                    Title </h4>
+                <span className='flex items-center gap-2' >
+                    <p className='text-lg font-semibold text-gray-800' >{data?.getScriptById.title}</p>
+                </span>
+            </div>
+            <hr />
+            <div className='flex gap-1 flex-col'>
+                <h4 className='text-md text-gray-600 font-medium flex items-center gap-1' >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                    Description </h4>
+                <span className='flex items-center gap-2' >
+                    <p className='text-lg font-semibold text-gray-800' >{data?.getScriptById.description}</p>
+                </span>
+            </div>
+            <hr />
+            <div className='flex gap-1 flex-col'>
+                <h4 className='text-md text-gray-600 font-medium flex items-center gap-1' >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
                     Author </h4>
                 <span className='flex items-center gap-2' >
                     <p className='text-lg font-semibold text-gray-800' >{data?.getScriptById.author.username}</p>
@@ -81,7 +103,7 @@ const ScriptDetails = () => {
                 <div className='flex gap-3 flex-wrap' >
                     {
                         data?.getScriptById.languages.map((language) => {
-                            return <p className='text-lg font-semibold text-gray-800 py-1 px-2 bg-gray-100 rounded-full' > {language}  </p>
+                            return <p className='text-lg font-semibold text-gray-800 py-1 px-2 bg-white rounded-xl' > {language}  </p>
                         })
                     }
                 </div>
@@ -98,7 +120,7 @@ const ScriptDetails = () => {
                 <div className='flex gap-3 flex-wrap' >
                     {
                         data?.getScriptById.genres.map((genre) => {
-                            return <p className='text-lg font-semibold text-gray-800 py-1 px-2 bg-gray-100 rounded-full' > # {genre}  </p>
+                            return <p className='text-lg font-semibold text-gray-800 py-1 px-2 bg-white rounded-xl' > # {genre}  </p>
                         })
                     }
                 </div>
